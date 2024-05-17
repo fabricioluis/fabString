7c3c07402dbc17f535bd82e6d2e7cb83a4b4a6fa
Fabricio Luis <ce3po.robo@gmail>
2024-05-17 17:08:03 -0300

    Inclui string com especificacao de tamanho.

--- main.cpp
+++ main.cpp
@@ -5,13 +5,17 @@ int main() {
   printf("Teste total com lib fabString.\n");
 
   char *a = (char *)malloc(50 * sizeof(char));
+  char *b = (char *)malloc(50 * sizeof(char));
+  char *temp = (char *)malloc(50 * sizeof(char));
+
+  //          1         2         3         4
+  // 1234567890123456789012345678901234567890
+  // GET /inclusao?nome=Maria&valor=123
+
   strcpy(a, "GET /inclusao?nome=Maria&valor=123");
 
   printf("%s\n", a);
 
-  char *temp = (char *)malloc(50 * sizeof(char));
-
-  temp = substr(a, 2, 3);
-  printf("%s\n", temp);
-  free(temp);
+  b = substr(a, 5, 9);
+  printf("%s\n", b);
 }
